06786ff3db6be2d212c5cba30a1e04edd4da1e91
r.michau <r.michau@criteo.com>
2022-11-05 09:24:39 +0100

    Support Queue feature
    - Allow to push async requests

--- cloudscraper/client.go
+++ cloudscraper/client.go
@@ -6,6 +6,7 @@ import (
 
 type CloudScrapper struct {
 	client        cycletls.CycleTLS
+	respChan      *chan cycletls.Response
 	defaultHeader map[string]string
 	ja3           string
 	userAgent     string
@@ -22,6 +23,22 @@ func (cs CloudScrapper) Do(url string, options cycletls.Options, method string) 
 		options.Ja3 = cs.ja3
 	}
 	return cs.client.Do(url, options, method)
+}
+
+// Queue Push a request in the Queue
+// Read the result with RespChan()
+func (cs CloudScrapper) Queue(url string, options cycletls.Options, method string) {
+	for k, v := range cs.defaultHeader {
+		options.Headers[k] = v
+	}
+	if options.UserAgent == "" {
+		options.UserAgent = cs.userAgent
+	}
+	if options.Ja3 == "" {
+		options.Ja3 = cs.ja3
+	}
+
+	cs.client.Queue(url, options, method)
 }
 
 func (cs CloudScrapper) Get(url string, headers map[string]string, body string) (cycletls.Response, error) {
@@ -42,12 +59,17 @@ func (cs CloudScrapper) Post(url string, headers map[string]string, body string)
 	return cs.Do(url, options, "POST")
 }
 
+func (cs CloudScrapper) RespChan() *chan cycletls.Response {
+	return cs.respChan
+}
+
 func Init(mobile bool) (*CloudScrapper, error) {
 	browserConf, err := getUserAgents(mobile)
 	if err != nil {
 		return nil, err
 	}
 	cycleTstClient := cycletls.Init()
-	p := CloudScrapper{client: cycleTstClient, defaultHeader: browserConf.Headers, ja3: browserConf.Ja3, userAgent: browserConf.UserAgent}
+	p := CloudScrapper{client: cycleTstClient, defaultHeader: browserConf.Headers, ja3: browserConf.Ja3, userAgent: browserConf.UserAgent,
+		respChan: &cycleTstClient.RespChan}
 	return &p, nil
 }
